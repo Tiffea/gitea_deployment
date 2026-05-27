e043936a57fa2167b78999fda2eecb5bdfbccc58
Tiffea <golovatenkoyan@gmail.com>
2026-05-27 20:16:43 +0300

    improve script

--- ansible/roles/scripts/files/health_check.sh
+++ ansible/roles/scripts/files/health_check.sh
@@ -1,10 +1,20 @@
-#!bin/bash
-if ! docker ps | grep gitea; then
+#!/bin/bash
+# read with /bin/bash
+
+PATH=/usr/local/bin:/usr/bin:/bin
+#from left to right (separated with :)
+cd /home/ubuntu/gitea_deployment || exit 1
+#open door or stop
+
+if ! docker ps | grep -q gitea; then
   docker compose up -d
 fi
+# if not true then container up
 
 
-if docker ps | grep gitea; then
-  echo "all is fine"; else
-  echo "smth is wrong"
+if docker ps | grep -q gitea; then
+  echo "working"
+else
+  echo "not working"
 fi
+#if not working say it
